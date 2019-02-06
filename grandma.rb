30a7db18cc0ed9cd105cacca94b80fc3275bd1c5
Adam Moran <jeffadammoran@gmail.com>
2019-02-06 21:05:22 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -15,7 +15,7 @@
 
 def speak_to_grandma (speak)
   if (speak == "I LOVE YOU GRANDMA!")
-    return "I LVOE YOU TOO PUMPKIN!"
+    return "I LOVE YOU TOO PUMPKIN!"
   elsif (speak == speak.upcase)
     return "NO, NOT SINCE 1938!"
   else
